5b32ccb52bd45eb784752b974c95c67d4b85a07d
dimasaddriansyah <dimasaddriansyah14@gmail.com>
2020-02-19 08:59:01 +0700

    Project SISKEU Dimas

--- index-user.php
+++ index-user.php
@@ -7,7 +7,7 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Sistem Keuangan POLINDRA</title>
+		<title>Sistem Keuangan POLINDRAA</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
